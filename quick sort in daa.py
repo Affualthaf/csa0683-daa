6e2d33b39b0cda6e34a813894d46c3643b015bf2
Affualthaf <154500904+Affualthaf@users.noreply.github.com>
2024-07-27 10:26:57 +0530

    Add files via upload

---
+++ quick sort in daa.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less_than_pivot = [x for x in arr[1:] if x <= pivot]
+        greater_than_pivot = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
+
+# Example Usage
+my_list = [29, 10, 14, 37, 13]
+sorted_list = quick_sort(my_list)
+print(sorted_list)
